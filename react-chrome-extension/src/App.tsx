2b5282a3608d3ed95bbb3499febdf5b1758fea6e
sun-jay <sunny.jyrm@gmail.com>
2023-06-07 14:43:28 -0700

    basic react extension

---
+++ react-chrome-extension/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container">
+      <button className="grow press" id="extractButton">
+          Add to AI Bookmarks!
+        </button>
+        <script src="popup.js"></script>
+      </div>
+
+  );
+}
+
+export default App;

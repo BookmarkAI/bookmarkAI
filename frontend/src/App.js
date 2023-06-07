a5726b10bedad5ae37ce91531ead8a551038f46a
sun-jay <sunny.jyrm@gmail.com>
2023-06-06 18:18:06 -0700

    basic firebase auth with login/out page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,37 +10,40 @@ import MobileFoldersScreen from './components/Mobile/MobileFoldersScreen';
 import MobileFolderView from "./components/Mobile/MobileFolderView";
 import React from 'react';
 import { FileProvider } from './utils/FileContext';
+import SignIn from './components/SignIn';
+import SignInPage from './pages/SignInPage';
+import SourceList from './components/SourceList';
+import Login from './components/SignIn';
+import { useEffect, useState } from 'react';
+import {auth} from './fb';
+
 
 
 function App() {
-  return (
-    <FileProvider>
-    <BrowserRouter>
-      <Desktop>
-        <Routes>
-          <Route path="/" element={<HomeScreen/>} />
-          <Route element={<Layout/>}>
-            <Route path="/browse" element={<BrowseScreen/>}/>
-            <Route path="/images" element={<BrowseImages/>}/>
-            <Route path="/search" element={<SearchResult/>}/>
-          </Route>
-        </Routes>
-      </Desktop>
 
-      <Mobile>
+  // this sets up the user state, its declared at the app level so we can pass it to all of the children components
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    })
+  }, [])
+  console.log(user);
+
+  return (
+    <div>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeScreen/>} />
+          <Route path="/" element={<Homescreen user={user}/>} />
           <Route element={<Layout/>}>
-            <Route path="/browse" element={<BrowseScreen/>}/>
-            <Route path="/images" element={<BrowseImages/>}/>
-            <Route path="/search" element={<SearchResult/>}/>
-            <Route path="/folders" element={<MobileFoldersScreen/>}/>
-            <Route path="/folder/:id" element={<MobileFolderView/>} />
+            <Route path="/signin" element={<SignInPage user={user}/>}/>
+            <Route path="/browse" element={<BrowseScreen user={user}/>}/>
+            <Route path="/images" element={<BrowseImages user={user}/>}/>
+            <Route path="/search" element={<SearchResult user={user}/>}/>
           </Route>
         </Routes>
-      </Mobile>
-    </BrowserRouter>
-    </FileProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 

34b305eec904bdfbbf356ce123ae1c56750c785f
sun-jay <sunny.jyrm@gmail.com>
2023-06-06 18:18:06 -0700

    basic firebase auth with login/out page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,36 +10,54 @@ import MobileFoldersScreen from './components/Mobile/MobileFoldersScreen';
 import MobileFolderView from "./components/Mobile/MobileFolderView";
 import React from 'react';
 import { FileProvider } from './utils/FileContext';
+import SignIn from './components/SignIn';
+import SignInPage from './pages/SignInPage';
+import SourceList from './components/SourceList';
+import Login from './components/SignIn';
+import { useEffect, useState } from 'react';
+import { auth } from './fb';
+
 
 
 function App() {
+
+  // this sets up the user state, its declared at the app level so we can pass it to all of the children components
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    })
+  }, [])
+  console.log(user);
+
   return (
     <FileProvider>
-    <BrowserRouter>
-      <Desktop>
-        <Routes>
-          <Route path="/" element={<HomeScreen/>} />
-          <Route element={<Layout/>}>
-            <Route path="/browse" element={<BrowseScreen/>}/>
-            <Route path="/images" element={<BrowseImages/>}/>
-            <Route path="/search" element={<SearchResult/>}/>
-          </Route>
-        </Routes>
-      </Desktop>
+      <BrowserRouter>
+        <Desktop>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route element={<Layout />}>
+              <Route path="/signin" element={<SignInPage user={user} />} />
+              <Route path="/browse" element={<BrowseScreen />} />
+              <Route path="/images" element={<BrowseImages />} />
+              <Route path="/search" element={<SearchResult />} />
+            </Route>
+          </Routes>
+        </Desktop>
 
-      <Mobile>
-        <Routes>
-          <Route path="/" element={<HomeScreen/>} />
-          <Route element={<Layout/>}>
-            <Route path="/browse" element={<BrowseScreen/>}/>
-            <Route path="/images" element={<BrowseImages/>}/>
-            <Route path="/search" element={<SearchResult/>}/>
-            <Route path="/folders" element={<MobileFoldersScreen/>}/>
-            <Route path="/folder/:id" element={<MobileFolderView/>} />
-          </Route>
-        </Routes>
-      </Mobile>
-    </BrowserRouter>
+        <Mobile>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route element={<Layout />}>
+              <Route path="/browse" element={<BrowseScreen />} />
+              <Route path="/images" element={<BrowseImages />} />
+              <Route path="/search" element={<SearchResult />} />
+              <Route path="/folders" element={<MobileFoldersScreen />} />
+              <Route path="/folder/:id" element={<MobileFolderView />} />
+            </Route>
+          </Routes>
+        </Mobile>
+      </BrowserRouter>
     </FileProvider>
   );
 }

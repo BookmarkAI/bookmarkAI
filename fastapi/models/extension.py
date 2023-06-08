8eb57f1fd2a7cd9392e8a3666d1ef827baaf7950
sun-jay <sunny.jyrm@gmail.com>
2023-06-08 13:51:11 -0700

    extension reads logged cookies from frontend

--- fastapi/models/extension.py
+++ fastapi/models/extension.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class ExtensionDocument(BaseModel):
     raw_text: str
     url: str
+    UID: str
+    title: str
+    image_urls: list
+    
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,8 @@ import SignInPage from './pages/SignInPage';
 import SourceList from './components/SourceList';
 import Login from './components/SignIn';
 import { useEffect, useState } from 'react';
-import {auth} from './fb';
+import { auth } from './fb';
+import { useCookies } from 'react-cookie';
 
 
 
@@ -19,9 +20,31 @@ function App() {
 
   // this sets up the user state, its declared at the app level so we can pass it to all of the children components
   const [user, setUser] = useState(null);
+  const [cookies, setCookie] = useCookies(["user"]);
   useEffect(() => {
     auth.onAuthStateChanged(user => {
+      // clear all cookies
+      document.cookie.split(";").forEach((c) => {
+        document.cookie = c
+          .replace(/^ +/, "")
+          .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+      });
+
       setUser(user);
+      setCookie(user ?
+        JSON.stringify({ identifier: "userCookie", displayName: user.displayName, uid: user.uid })
+        : JSON.stringify({ identifier: "userCookie", displayName: null, uid: null }));
+
+      // we can basically authenticate the extension by setting a cookie here
+      // and accessing it in the extension
+      // we can store a hashed version of the user's firebase uid in cookies, which the extension can access
+      // and send in its requests to the backend
+
+      // there is a security vulnerability here, where someone can access the cookie 
+      // and send a request to the backend
+
+
+
     })
   }, [])
   console.log(user);
@@ -30,12 +53,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homescreen user={user}/>} />
-          <Route element={<Layout/>}>
-            <Route path="/signin" element={<SignInPage user={user}/>}/>
-            <Route path="/browse" element={<BrowseScreen user={user}/>}/>
-            <Route path="/images" element={<BrowseImages user={user}/>}/>
-            <Route path="/search" element={<SearchResult user={user}/>}/>
+          <Route path="/" element={<Homescreen user={user} />} />
+          <Route element={<Layout />}>
+            <Route path="/signin" element={<SignInPage user={user} />} />
+            <Route path="/browse" element={<BrowseScreen user={user} />} />
+            <Route path="/images" element={<BrowseImages user={user} />} />
+            <Route path="/search" element={<SearchResult user={user} />} />
           </Route>
         </Routes>
       </BrowserRouter>

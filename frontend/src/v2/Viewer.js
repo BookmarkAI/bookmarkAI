0d2e2fcb6232dd4297db04f7d64a4848b1bd6106
Kyuhee Jo <kyuhee0622@gmail.com>
2023-06-29 18:38:01 -0700

    loading: needs improvement

--- frontend/src/v2/Viewer.js
+++ frontend/src/v2/Viewer.js
@@ -6,21 +6,40 @@ import { Typography } from "@mui/material";
 import { displayUrl } from "./utils";
 import LaunchIcon from '@mui/icons-material/Launch';
 import Stack from '@mui/material/Stack'
+import LinearProgress from "@mui/material/LinearProgress";
+import { useState } from "react";
+import { useEffect } from "react";
 
 // var perf =require('./test.html');
 
 export default function Viewer({url, setViewer}) {
+  const [iframeLoaded, setIframeLoaded] = useState(false);
+
+  const handleIframeLoad = () => {
+    setTimeout(() => {
+      // Change the button icon back to the original icon
+      setIframeLoaded(true);
+  }, 2000); // 10 seconds in milliseconds
+    
+  };
+
+  useEffect(() => {
+    setIframeLoaded(false);
+    handleIframeLoad()
+  }, [url]);
+
     return (
         <Box component="main" sx={{ flexGrow: 1,  height: '100vh', borderRight: 1}}>
         <DrawerHeader />
         <Box sx={{width: '100%', height: 30, p: 1.5, mt: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'flex-end'}}> <LanguageOutlinedIcon fontSize="small" sx={{mr:1}}/> <Typography sx={{fontSize: 12}}> {displayUrl(url)} </Typography> </Box>
+          <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'flex-end'}}> <LanguageOutlinedIcon fontSize="small" sx={{mr:1, color: '#414250'}}/> <Typography sx={{fontSize: 12}}> {displayUrl(url)} </Typography> </Box>
           <Stack direction="row" spacing={1}>
-            <LaunchIcon onClick={()=>{window.open(url)}} fontSize="small"/>
-            <CloseOutlinedIcon onClick={()=>setViewer(null)} fontSize="small"/>
+            <LaunchIcon onClick={()=>{window.open(url)}} fontSize="small" sx={{color: '#414250'}}/>
+            <CloseOutlinedIcon onClick={()=>setViewer(null)} fontSize="small" sx={{color: '#414250'}}/>
           </Stack>
         </Box>
-        <iframe width="100%" height='88.5%' src={`http://localhost:3000/api/viewer?url=${url}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+         {!iframeLoaded && <LinearProgress/>}
+        <iframe position="relative" zIndex={1000} width="100%" height='88.5%' src={`http://localhost:3000/api/viewer?url=${url}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Box>
     )
 }
